d1b5cba77e0b2d7bf158a5cfe27218ab805a5d9b
Jakub <rutrle@seznam.cz>
2021-11-06 17:45:13 +0100

    adding python function to flask

--- first_flask_app.py
+++ first_flask_app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from vsearch import search4letters
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello() -> str:
-    return 'Hello world from Flask!'
+    return 'Hello world from FLask!'
 
 
-app.run()
+@app.route('/search4')
+def do_search() -> str:
+    return str(search4letters('Hello world from FLask!'))
+
+
+app.run(port=5000)

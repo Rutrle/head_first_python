6bdbf00ae9ade4a7cb90f73b261dcf4b5b6391db
Jakub <rutrle@seznam.cz>
2021-11-14 23:23:03 +0100

    multiple urls to one function

--- vsearch4web.py
+++ vsearch4web.py
@@ -4,21 +4,21 @@ from vsearch import search4letters
 app = Flask(__name__)
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page():
     return render_template('entry.html', the_title='welcome to search4letter on the web!')
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from FLask!'
-
-
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
+    title = "Here are your results"
     word = request.form['phrase']
     letters = request.form['letters']
-    return str(search4letters(word, letters))
+
+    results = str(search4letters(word, letters))
+    return render_template('result.html', the_title=title, the_phrase=word, the_letters=letters, the_results=results)
 
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)

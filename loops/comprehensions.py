27da73402248dc6c1f261f279de9ae0e7b2b3155
Jakub Rutrle <rutrle@seznam.cz>
2022-01-12 00:45:43 +0100

    comprehension with if

--- loops/comprehensions.py
+++ loops/comprehensions.py
@@ -1,20 +1,23 @@
 import pprint
 from datetime import datetime
 
-def convert2ampm(time24:str):
-    return datetime.strptime(time24,'%H:%M').strftime('%I:%M%p')
+
+def convert2ampm(time24: str):
+    return datetime.strptime(time24, '%H:%M').strftime('%I:%M%p')
+
 
 with open('buzzers.csv', mode='r') as data:
     ignore = data.readline()
     flights = {}
     for line in data:
-        k,v = line.strip().split(',') # strip takes away /n and other leading or trailing whitespaces
-        flights[k]=v    
+        # strip takes away /n and other leading or trailing whitespaces
+        k, v = line.strip().split(',')
+        flights[k] = v
     pprint.pprint(flights)
 
-    flights2={}
+    flights2 = {}
     for key, value in flights.items():
-        flights2[convert2ampm(key)] = value.title() 
+        flights2[convert2ampm(key)] = value.title()
 
     pprint.pprint(flights)
     pprint.pprint(flights2)
@@ -22,10 +25,16 @@ with open('buzzers.csv', mode='r') as data:
     destinations = [x.title() for x in flights.values()]
     print(destinations)
 
-    more_flights = {convert2ampm(k):v.title() for k,v in flights.items()}
+    more_flights = {convert2ampm(k): v.title() for k, v in flights.items()}
 
     print(more_flights)
 
-#comprehensions are faster than for loops!
+    # comprehensions are faster than for loops!
+    just_freeport2 = {convert2ampm(k): v.title()
+                      for k, v in flights.items() if v == 'FREEPORT'}
+    pprint.pprint(just_freeport2)
 
-#535
+    fts = {convert2ampm(k): v.title() for k, v in flights.items()}
+
+
+# 535
